09b428898cfea38d82ce2ae020bbe8af781142bc
Frederick Griffin <fcgriffin2@outlook.com>
2023-01-17 13:44:40 -0500

    fixing what i broke

--- EntryBOX.py
+++ EntryBOX.py
@@ -6,7 +6,10 @@ root.iconbitmap('apple.ico')
 root.geometry('500x500')
 
 #entry is an entry box that operated within root
-e = Entry(root, width=50, fg='black').pack()
+#if you try to do a one liner here, you set e to the result of the .pack() command
+#this in turn makes it so that you cannot access the internal portions of the entry
+e = Entry(root, width=50, fg='black')
+e.pack()
 
 def my_click():
     my_label = Label(root, text='hello ' + e.get()).pack()

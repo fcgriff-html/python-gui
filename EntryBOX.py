ff2ad85d3ef44f569a737b9764c206eace67fdf5
Frederick Griffin <fcgriffin2@outlook.com>
2023-01-17 13:32:44 -0500

    please work please

--- EntryBOX.py
+++ EntryBOX.py
@@ -6,6 +6,12 @@ root.iconbitmap('apple.ico')
 root.geometry('500x500')
 
 #entry is an entry box that operated within root
-e = Entry(root, width=50, fg='red').pack()
+e = Entry(root, width=50, fg='black').pack()
+
+def my_click():
+    my_label = Label(root, text='hello ' + e.get()).pack()
+    #the above grabs the text from the entry and displays it
+
+my_button = Button(root, text='Enter your name', padx=10, pady=10, bg='white', fg='green', command=my_click).pack()
 
 root.mainloop()

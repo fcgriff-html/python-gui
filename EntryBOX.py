4fc3339421a5bb67feab63d08a29c150f7305682
Frederick Griffin <fcgriffin2@outlook.com>
2023-01-17 14:03:29 -0500

    entry box complete thus far

--- EntryBOX.py
+++ EntryBOX.py
@@ -9,12 +9,28 @@ root.geometry('500x500')
 #if you try to do a one liner here, you set e to the result of the .pack() command
 #this in turn makes it so that you cannot access the internal portions of the entry
 e = Entry(root, width=50, fg='black')
-e.pack()
+e.grid(row=0, column=1)
+#you can say .pack(), but .grid will alow you to organize where the textbox appears
+#grid also prevents the screen from filling with the result
 
+ee = Entry(root, width=30, fg="black")
+ee.grid(row=0, column=2)
 def my_click():
-    my_label = Label(root, text='hello ' + e.get()).pack()
+    my_label = Label(root, text='hello ' + e.get()).grid(row=3, column=1)
     #the above grabs the text from the entry and displays it
+    e.delete(0, END)
 
-my_button = Button(root, text='Enter your name', padx=10, pady=10, bg='white', fg='green', command=my_click).pack()
+def my_click2():
+    my_label2 = Label(root, text='hello ' + ee.get()).grid(row=3, column=2)
+    ee.delete(0, END)
+
+my_button = Button(root, text='Enter your first name', padx=10, pady=10, bg='white', fg='green', command=my_click)
+my_button.grid(row=2, column=1)
+
+my_button2 = Button(root, text='Enter your last name', padx=10, pady=10, bg='white', fg='green', command=my_click2)
+my_button2.grid(row=2, column=2)
+
+
+#because there is a pack() command we cant use grid, so we have to adjust the pack to grid
 
 root.mainloop()
